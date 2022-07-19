382839154503ffc7582a5c23db41b5f77628e0b5
mustenaka <mumten120@outlook.com>
2022-07-19 22:47:30 +0800

    [feat] add reflect function test

--- simpleSort.go
+++ simpleSort.go
@@ -15,33 +15,56 @@ func (a SortBy) Less(i, j int) bool {
 	// fmt.Println(reflect.ValueOf(a[i]).Interface())
 
 	// dataType := reflect.TypeOf(a[i])
-	dataValue := reflect.ValueOf(a[i])
+	// dataValue := reflect.ValueOf(a[i])
 
 	for ii := 0; ii < reflect.TypeOf(a[i]).NumField(); ii++ {
-		filed := dataValue.Field(ii)
-		// filedName := filed.Type().Name()
-		// filedValue := dataValue.FieldByName(filedName)
-		fmt.Println(filed)
-		fmt.Println("-------------------")
+		// filed := dataValue.Field(ii)
+		// // filedName := filed.Type().Name()
+		// // filedValue := dataValue.FieldByName(filedName)
+		// fmt.Println(filed)
+		// fmt.Println("-------------------")
 	}
 
 	return false
 }
 
-// 默认简单排序
-func Simplesort(args interface{}) ([]interface{}, error) {
+// @title Simplesort
+// @description simplified non-generic sort method.
+// @param data interface{} "Data to be sorted."
+// @param filedName string "sorted index field name."
+// @param order bool "oder or reverse order"
+// @return interface{}, error "sort results, nil if no error."
+func Simplesort(args interface{}, filedName string, order bool) ([]interface{}, error) {
 	// 输出数据
 	fmt.Println("input: ", args)
 
-	// 将interface{}转换为[]interface{}
+	// interface{} convert to []interface{}
 	var interfaceSlice []interface{}
-	if reflect.TypeOf(args).Kind() == reflect.Slice {
-		fmt.Println("args typeof kind: " + reflect.TypeOf(args).Kind().String())
-		// fmt.Println(reflect.TypeOf(args).NumField())
-		s := reflect.ValueOf(args)
-		for i := 0; i < s.Len(); i++ {
-			ele := s.Index(i)
-			interfaceSlice = append(interfaceSlice, ele.Interface())
+
+	// 判断类型
+	if reflect.TypeOf(args).Kind() != reflect.Slice {
+		panic("input need slice kind")
+	}
+
+	// 输出类型信息
+	fmt.Println("args typeof kind: " + reflect.TypeOf(args).Kind().String())
+
+	// 切片处理
+	s := reflect.ValueOf(args)
+	for i := 0; i < s.Len(); i++ {
+		ele := s.Index(i)
+		t := ele.Type()
+
+		interfaceSlice = append(interfaceSlice, ele.Interface())
+
+		for ii := 0; ii < ele.NumField(); ii++ {
+			fmt.Printf("name: %s, type: %s, value: %v\n",
+				t.Field(ii).Name,
+				ele.Field(ii).Type(),
+				ele.Field(ii))
+			if t.Field(ii).Name == filedName {
+				fmt.Println("find filed: " + filedName)
+			}
 		}
 	}
 
@@ -51,4 +74,32 @@ func Simplesort(args interface{}) ([]interface{}, error) {
 	// 输出数据
 	fmt.Println("output: ", interfaceSlice)
 	return interfaceSlice, nil
+}
+
+// @title SimplesortStable
+// @description reflect test
+func FunctionTest(data interface{}) {
+	getValue := reflect.ValueOf(data) // Value of v
+	if getValue.Kind() != reflect.Slice {
+		panic("need slice kind")
+	}
+
+	l := getValue.Len()
+	for i := 0; i < l; i++ {
+		value := getValue.Index(i) // Value of item
+		typel := value.Type()      // Type of item
+		if typel.Kind() != reflect.Struct {
+			panic("need struct kind")
+		}
+
+		fmt.Printf("type-kind: %s, type-name: %s, value: %v\n", typel.Kind(), typel.Name(), value.Interface())
+
+		num := value.NumField()
+		for j := 0; j < num; j++ {
+			fmt.Printf("name: %s, type: %s, value: %v\n",
+				typel.Field(j).Name,
+				value.Field(j).Type(),
+				value.Field(j))
+		}
+	}
 }
