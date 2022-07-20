d08550efc0b3e68d8fb84b95611f2de72bae92c9
mustenaka <mumten120@outlook.com>
2022-07-20 18:04:09 +0800

    [feat] add order dield

--- sortConfig/sortConfig.go
+++ sortConfig/sortConfig.go
@@ -1,8 +1,9 @@
-package sortconfig
+package sortConfig
 
 type sortConfig struct {
 	index int    // 字段索引
 	name  string // 字段名称
+	order bool   // 排序方式 - true顺序，false逆序
 }
 
 type SortConfig interface {
@@ -14,11 +15,27 @@ type SortConfig interface {
 
 var _ SortConfig = (*sortConfig)(nil)
 
+var instance *sortConfig
+
+// 获取单例
+func CreateInstance(index int, name string, order bool) *sortConfig {
+	if instance == nil {
+		instance = NewSortConfig(index, name, order)
+	}
+	return instance
+}
+
+// 获取单例
+func GetInstance() *sortConfig {
+	return instance
+}
+
 // 初始化
-func NewSortConfig(index int, name string) *sortConfig {
+func NewSortConfig(index int, name string, order bool) *sortConfig {
 	return &sortConfig{
 		index: index,
 		name:  name,
+		order: order,
 	}
 }
 
@@ -28,4 +45,8 @@ func (conf *sortConfig) GetIndex() int {
 
 func (conf *sortConfig) GetName() string {
 	return conf.name
+}
+
+func (conf *sortConfig) GetOrder() bool {
+	return conf.order
 }
